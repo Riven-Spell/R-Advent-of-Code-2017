4a851c36592ccbe23894911ef756b72f9f04d51d
Adele Reed <privacyradiator@gmail.com>
2017-12-08 04:43:11 -0800

    5_2 improved

--- Day5.R
+++ Day5.R
@@ -1,21 +1,9 @@
-jumpMaze <- function(x = "") {
+jumpMaze <- function(x = "", v = 1) {
   jumps <- as.numeric(unlist(strsplit(x,"\n")))
   stackptr <- 1
   i <- 0
   while(stackptr <= length(jumps)) {
-    jumps[stackptr] <- jumps[stackptr] + 1
-    stackptr <- stackptr + jumps[stackptr] - 1
-    i <- i + 1
-  }
-  i
-}
-
-jumpMaze2 <- function(x = "") {
-  jumps <- as.numeric(unlist(strsplit(x,"\n")))
-  stackptr <- 1
-  i <- 0
-  while(stackptr <= length(jumps)) {
-    inc <- if(jumps[stackptr] >= 3) -1 else 1
+    inc <- if(jumps[stackptr] >= 3 && v == 2) -1 else 1
     jumps[stackptr] <- jumps[stackptr] + inc
     stackptr <- stackptr + jumps[stackptr] - inc
     i <- i + 1
