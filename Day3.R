e66aaddff3bf0c77638fe2f4212622f7bbb14578
Adele Lynn <virepri2k@gmail.com>
2017-12-08 01:22:34 -0800

    3_2 closer to working

--- Day3.R
+++ Day3.R
@@ -16,36 +16,56 @@ findSteps1 <- function(x = 1) {
 
 # STEP 2
 # NEEDS FIXING
-findNext <- function(x = 1) {
-  lastLayer <- c(1)
-  thisLayer <- numeric(0)
-  layerRoot <- 3
-  while(length(thisLayer) == 0 || thisLayer[length(thisLayer)] < x) {
-    if(length(thisLayer) > ((layerRoot * 2) + ((layerRoot - 2) * 2))) {
-      lastLayer <- thisLayer
-      thisLayer <- numeric(0)
-      layerRoot <- layerRoot + 2
+findNext <- function(x = 747) {
+  m <- matrix(0,3,3)
+  m[2,2] <- 1
+  cursor <- c(2,2)
+  i <- 1
+  root <- 1
+  perim <- findPerim(root)
+  actions <- list(
+    c(-1,0), # First quarter, move upwards
+    c(0,-1), # Second quarter, move left
+    c(1,0), # Third quarter, move downwards
+    c(0,1), # Final quarter, move right.
+    c(0,0) # sanity check
+  )
+  
+  while(length(m[m <= x]) != 1) {
+    if(root^2 <= i) { # Iterated root squared times
+      ml <- length(m[1,]) # Get size of matrix
+      tmpm <- matrix(0,ml + 2,ml + 2) # Create matrix 2 larger
+      tmpm[2:(2+ml-1),2:(2+ml-1)] <- m # Place matrix in middle of temp matrix
+      m <- tmpm # Set matrix to temp matrix
+      cursor <- cursor + c(1,2) # Move cursor to old position + (x + 1)
+      root <- root + 2 # Iterate the root upwards one step.
+      perim <- findPerim(root)
     }
-    print(thisLayer)
     
-    if(length(thisLayer) == 0) {
-      if(length(lastLayer) != 1) {
-        thisLayer <- append(thisLayer, lastLayer[1] + lastLayer[length(lastLayer)])
-      } else {
-        thisLayer <- append(thisLayer, lastLayer[1])
-      }
-    } else {
-      thisLayer <- append(thisLayer, thisLayer[length(thisLayer)] + sum(lastLayer[lastLayerRange(lastLayer, thisLayer, layerRoot)]))
-    }
+    # Calculate & assign new block.
+    m[cursor[1],cursor[2]] <- sum(m[(cursor[1]-1):(cursor[1]+1),(cursor[2]-1):(cursor[2]+1)]) - m[cursor[1],cursor[2]]
+    # Move cursor.
+    lastroot <- root - 2 # determine last root to find current progression to next root
+    tmpi <- i - (lastroot^2) # determine progression to next root
+    perimq <- perim / 4 # perimiter into quarters
+    
+    #debug
+    print(m)
+    print(cursor)
+    print("Action:")
+    print(actions[[((tmpi + 1) %/% perimq) + 1]])
+    print("Maximum:")
+    print(max(unlist(m)))
+    print("Iteration:")
+    print(i)
+    
+    
+    cursor <- cursor + actions[[(tmpi %/% perimq) + 1]] # mouse performs the closest 25% action.
+    i <- i + 1
   }
-  thisLayer[length(thisLayer)]
+  m[m <= x]
 }
 
-# WORKS
-lastLayerRange <- function(lastLayer = 1, thisLayer = numeric(0), layerRoot = 3) {
-  posTL <- (length(thisLayer) - 1) %% (layerRoot - 1) + 1
-  base <- (pmax(1, (posTL - 1)):pmin(layerRoot - 2, (posTL + 1)) - 1)
-  tlq <- ((length(thisLayer) - 1) %/% (layerRoot - 1))
-  llq <- (length(lastLayer) %/% 4)
-  base + (tlq * llq)
+findPerim <- function(x = 3) {
+  (x * 2) + ((x - 2) * 2)
 }
